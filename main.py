8de5d0c45c44defd7b38cdd86ce6d60ae05aff6b
CZ <dev.zombek@gmail.com>
2020-08-15 16:22:38 -0400

    Final final AWSHTML push - added print all buckets

--- main.py
+++ main.py
@@ -29,10 +29,18 @@ class AWSManager:
         self.s3_client.upload_file('chandtroduction.html','lmtd-class','chandlerzombek.html')
     def load_from_s3(self):
         self.s3_client.download_file('lmtd-class','chandlerzombek.html','localchandlerzombek.html')
+    def list_all_buckets(self):
+        print(self.s3_client.list_buckets())
+    def print_all_objects(self):
+        objects = self.s3_client.list_objects(
+            Bucket = "lmtd-class"
+        )
+        print(objects)
 
 s1 = HtmlDocument()
 s1.save()
 
 s2 = AWSManager()
 s2.save_to_s3()
-s2.load_from_s3()
+#s2.load_from_s3()
+s2.print_all_objects()
