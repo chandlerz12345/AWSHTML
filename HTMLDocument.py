d65c6394da85ff836dd38ef747b8399214c70df6
CZ <dev.zombek@gmail.com>
2020-08-15 14:51:23 -0400

    Pre-AWSManager

--- HTMLDocument.py
+++
@@ -1,4 +0,0 @@
-boto3.client = dictionaries
-boto3.resource = objects get retunred back
-
-boto3resource(s3).meta.client
---
+++ main.py
@@ -0,0 +1,30 @@
+import webbrowser
+import boto3
+
+
+
+class HtmlDocument:
+    def __init__(self):
+        self.message = "<html> <head></head> <body><p>Hello World!</p></body> </html> "
+        self.save1 = HtmlManager()
+
+    def save(self):
+        self.to_save = self.message 
+        self.save1.writehtml(self.to_save)
+       
+
+class HtmlManager:
+    def __init__(self):
+        pass
+    def writehtml(self, message):
+        samplehtml = open('helloworld.html', 'w')
+        samplehtml.write(message)
+        samplehtml.close()
+        webbrowser.open_new_tab('helloworld.html')
+
+class AWSManager:
+    def __init__(self):
+        pass
+
+s1 = HtmlDocument()
+s1.save()
